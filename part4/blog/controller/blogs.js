43540fd7df2a8e67330d74c34172f82bd07b4319
Uncle Liu <18643094191@163.com>
2019-12-19 13:58:55 +0800

    hw 4.19 finished.

--- part4/blog/controller/blogs.js
+++ part4/blog/controller/blogs.js
@@ -1,28 +1,50 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+	const authorization = request.get('authorization')
+	if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+		return authorization.substring(7)
+	}
+	return null
+}
 
 blogRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user')
     response.json(blogs.map( blog => blog.toJSON() ))
 })
 
-blogRouter.post('/', async (request, response) => {
-    // const user = await User.findOne({  })
-    const { title, author, url, likes, userId } = request.body
-    const user = await User.findById(userId)    
-    const blog = new Blog({
-        title,
-        author,
-        url,
-        likes,
-        user: user._id,
-    })
+blogRouter.post('/', async (request, response, next) => {
+    const body = request.body
+    const token = getTokenFrom(request)
 
-    const savedBlog = await blog.save()
-    user.blogs = user.blogs.concat(savedBlog._id)
-    await user.save()
-    response.json(savedBlog.toJSON())
+    try {
+        const decodedToken = jwt.verify(token, process.env.SECRET)
+		if (!token || !decodedToken.id) {
+			return response.status(401).json({
+				error: 'token missing or invalid'
+			})
+		}
+        const { title, author, url, likes, userId } = body
+        const user = await User.findById(userId)
+        const blog = new Blog({
+            title,
+            author,
+            url,
+            likes,
+            user: user._id,
+        })
+
+        const savedBlog = await blog.save()
+        user.blogs = user.blogs.concat(savedBlog._id)
+        await user.save()
+        response.json(savedBlog.toJSON())
+
+    } catch(exception) {
+        next(exception)
+    }
 })
 
 module.exports = blogRouter
